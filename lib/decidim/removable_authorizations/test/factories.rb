0012df145f1342def4dd69e8f220ceaec9ad87cd
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-10-30 22:44:03 +0100

    Remove useless factory

--- lib/decidim/removable_authorizations/test/factories.rb
+++ lib/decidim/removable_authorizations/test/factories.rb
@@ -1,13 +1,3 @@
 # frozen_string_literal: true
 
 require "decidim/core/test/factories"
-
-FactoryBot.define do
-  factory :removable_authorizations_component, parent: :component do
-    name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :removable_authorizations).i18n_name }
-    manifest_name :removable_authorizations
-    participatory_space { create(:participatory_process, :with_steps) }
-  end
-
-  # Add engine factories here
-end
