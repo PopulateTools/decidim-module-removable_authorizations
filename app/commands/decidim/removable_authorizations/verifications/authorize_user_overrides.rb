d47681c0c1427f33947f9bcd2e5be336ae95af94
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2026-04-01 12:37:36 +0200

    Adapt AuthorizeUser overrides to 0.30 whit ephemeral feature

--- app/commands/decidim/removable_authorizations/verifications/authorize_user_overrides.rb
+++ app/commands/decidim/removable_authorizations/verifications/authorize_user_overrides.rb
@@ -4,8 +4,27 @@ module Decidim
   module RemovableAuthorizations
     module Verifications
       module AuthorizeUserOverrides
+        # Executes the command. Broadcasts these events:
+        #
+        # - :ok when everything is valid.
+        # - :invalid if the handler was not valid and we could not proceed.
+        # - :transferred if there is a duplicated authorization associated
+        #                to other user and the authorization can be
+        #                transferred.
+        # - :transfer_user if there is a duplicated authorization associated
+        #                  to an ephemeral user and the current user is also
+        #                  ephemeral the session is transferred to the user
+        #                  with the existing authorization
+        #
+        # Returns nothing.
         def call
-          return transfer_authorization if !handler.unique? && handler.transferrable?
+          if !handler.unique? && handler.user_transferrable?
+            handler.user = handler.duplicate.user
+            Authorization.create_or_update_from(handler)
+            return broadcast(:transfer_user, handler.user)
+          end
+
+          return transfer_authorization_with_log if !handler.unique? && handler.transferrable?
 
           if handler.invalid?
             register_conflict
@@ -13,7 +32,10 @@ module Decidim
             return broadcast(:invalid)
           end
 
+          return broadcast(:invalid) unless set_tos_agreement
+
           Authorization.create_or_update_from(handler)
+
           handler.log_successful_authorization
 
           broadcast(:ok)
